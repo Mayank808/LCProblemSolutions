866456e84f596c9031a098998b38e919472e630e
Mayank Mehra <70068077+Mayank808@users.noreply.github.com>
2024-04-26 13:25:40 -0400

    Create TwoSum.py

---
+++ NeetCode150/TwoSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for index, x in enumerate(nums):
+            if target - x in seen: 
+                return [seen[target - x], index]
+            
+            seen[x] = index
+        
+
+        return [-1, -1]
